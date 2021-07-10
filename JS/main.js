7434a00040f44eee1fc2ce2118bdbc92ee4ea828
daniel dantas <dan_dantas@hotmail.com>
2021-07-09 21:30:47 -0300

    Commit inicial Contador Santander Coders Live

---
+++ JS/main.js
@@ -0,0 +1,64 @@
+function  botao(){
+    alert("Obrigado por clicar!");
+}
+
+/*function soma(n1,n2){
+    return n1+n2;
+}
+alert(soma(2,10));
+var validar;
+function validaIdade(idade){
+    if(idade >=18){
+        validar=true
+    }else{
+        validar=false
+    }
+    return validar;
+}
+var idade= prompt("Qual a sua idade?");
+validaIdade(idade)
+console.log(validar);
+*/
+/*var d = new Date
+alert(d.getHours());
+alert(d.getDate());
+/*var count = 0;
+while (count <= 5){
+    console.log(count);
+    alert(count);
+    count++
+}
+*/
+/*var idade = prompt("Qual sua idade?");
+if (idade >= 18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade")
+}*/
+/*var frutas = [{nome: "maçã", cor: "vermelha"},{nome:"uva", cor:"roxa"}]
+console.log(frutas);
+alert(frutas[1].nome);
+alert(frutas[0].nome);*/
+/*var fruta = {nome: "maçã", cor: "vermelha"}
+console.log(fruta.nome)
+alert(fruta.cor)*/
+/* var nome= "Dan";
+var n1= 49;
+var n2= 10;
+var lista =["maçã","pêra","laranja"];
+var frase = "Japão é o melhor time do mundo";
+alert(frase.replace("Japão","Brasil"));
+alert("Bem vindo "+ nome +" "+ (n1 + n2));
+console.log(nome);
+console.log(n1 * n2);
+console.log(frase.toUpperCase());
+console.log(lista);
+lista.push("uva");
+//console.log(lista);
+lista.pop();
+alert(lista);
+//alert(lista);
+alert(frase.toUpperCase()); */
+
+
+
